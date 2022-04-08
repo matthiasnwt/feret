7883ed048304736da84a8f5c95528914dc498493
matthiasnwt <62239991+matthiasnwt@users.noreply.github.com>
2022-04-08 23:21:40 +0200

    small updated

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 # This call to setup() does all the work
 setup(
     name="feret",
-    version="0.1.0",
+    version="0.1.1",
     description="Calculates the maximum and minimum feret diameter",
     long_description=long_description,
     long_description_content_type="text/markdown",
